1eeeaa2b20a1a4b0f47a19134283c01d7c5239ac
Furkan Işıtan <furkanisitan96@gmail.com>
2021-06-24 04:35:07 +0300

    Create TestValidator.cs

---
+++ tests/FluentValidation.CustomPropertyValidators.Tests/TestValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FluentValidation.CustomPropertyValidators.Tests
+{
+    internal class TestValidator : InlineValidator<User>
+    {
+        public new CascadeMode CascadeMode
+        {
+            get => base.CascadeMode;
+            set => base.CascadeMode = value;
+        }
+
+        public TestValidator() { }
+
+        public TestValidator(params Action<TestValidator>[] actions)
+        {
+            foreach (var action in actions)
+                action(this);
+        }
+    }
+}
