1bfe49fed482b14d230e7ffb855174f19ec24e6c
Furkan Işıtan <furkanisitan96@gmail.com>
2021-06-24 04:33:36 +0300

    Create UniqueValidator.cs

---
+++ src/FluentValidation.CustomPropertyValidators/UniqueValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation.Validators;
+using System;
+
+namespace FluentValidation.CustomPropertyValidators
+{
+    public class UniqueValidator<T, TProperty> : PropertyValidator<T, TProperty>
+    {
+        public override bool IsValid(ValidationContext<T> context, TProperty value)
+        {
+            // TODO Implement this method
+            throw new NotImplementedException();
+        }
+
+        public override string Name => "UniqueValidator";
+    }
+}
