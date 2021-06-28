5abcb483a66802ffac39d481ef62c7eccbce97c4
Furkan Işıtan <furkanisitan96@gmail.com>
2021-06-28 09:34:46 +0300

    Create result interfaces

---
+++ src/Core/Utilities/Results/Abstracts/IResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Utilities.Results.Abstracts
+{
+    public interface IResult
+    {
+        bool Success { get; set; }
+        string Message { get; set; }
+
+    }
+
+    public interface IResult<T> : IResult
+    {
+        T Data { get; set; }
+    }
+}
---
+++ src/Core/Utilities/Results/Abstracts/IServiceResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Utilities.Results.Abstracts
+{
+    public interface IServiceResult : IResult
+    {
+        public int StatusCode { get; set; }
+    }
+
+    public interface IServiceResult<T> : IServiceResult, IResult<T>
+    {
+    }
+}
