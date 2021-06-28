3cc253d7b40121af48ff9cbca4a7b8fc0cf89c84
Furkan Işıtan <furkanisitan96@gmail.com>
2021-06-28 09:36:19 +0300

    Create result implementation classes

---
+++ src/Core/Utilities/Results/ServiceResult.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities.Results.Abstracts;
+using Core.Utilities.Results.Builders;
+
+namespace Core.Utilities.Results
+{
+    public class ServiceResult : Result, IServiceResult
+    {
+        public int StatusCode { get; set; }
+
+        public new static ServiceResultBuilder<object> Builder() => new();
+
+        public new static ServiceResultBuilder<T> Builder<T>(T data) => new(data);
+    }
+
+    public class ServiceResult<T> : ServiceResult, IServiceResult<T>
+    {
+        public T Data { get; set; }
+    }
+}
