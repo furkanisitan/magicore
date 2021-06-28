50cc6cb57fbd8412a41ee6bb87c5209c4dfbe7c2
Furkan Işıtan <furkanisitan96@gmail.com>
2021-06-28 09:35:38 +0300

    Create result builders

---
+++ src/Core/Utilities/Results/Builders/ResultBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Utilities.Results.Builders
+{
+    public class ResultBuilder<T> : AbstractResultBuilder<Result<T>, ResultBuilder<T>, T>
+    {
+        public ResultBuilder() { }
+
+        public ResultBuilder(T data) : base(data) { }
+    }
+
+}
---
+++ src/Core/Utilities/Results/Builders/ServiceResultBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Utilities.Results.Builders
+{
+    public class ServiceResultBuilder<T> : AbstractResultBuilder<ServiceResult<T>, ServiceResultBuilder<T>, T>
+    {
+        public ServiceResultBuilder() { }
+
+        public ServiceResultBuilder(T data) : base(data) { }
+
+        public ServiceResultBuilder<T> StatusCode(int statusCode)
+        {
+            Result.StatusCode = statusCode;
+            return Builder;
+        }
+
+    }
+}
