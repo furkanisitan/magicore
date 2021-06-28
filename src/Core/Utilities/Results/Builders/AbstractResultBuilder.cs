b38105117e90c683f2cc2d14e6dfa23cf44d9016
Furkan Işıtan <furkanisitan96@gmail.com>
2021-06-28 09:35:12 +0300

    Create AbstractResultBuilder.cs

---
+++ src/Core/Utilities/Results/Builders/AbstractResultBuilder.cs
@@ -0,0 +1,42 @@
+﻿using Core.Utilities.Results.Abstracts;
+using System;
+
+namespace Core.Utilities.Results.Builders
+{
+    public abstract class AbstractResultBuilder<TResult, TBuilder, TData>
+        where TResult : class, IResult<TData>, new()
+        where TBuilder : AbstractResultBuilder<TResult, TBuilder, TData>
+    {
+
+        protected readonly TBuilder Builder;
+        protected readonly TResult Result;
+
+        protected AbstractResultBuilder()
+        {
+            Builder = (TBuilder)this;
+            Result = Activator.CreateInstance<TResult>();
+        }
+
+        protected AbstractResultBuilder(TData data) : this()
+        {
+            Result.Data = data;
+        }
+
+        public TBuilder Success(bool success = true)
+        {
+            Result.Success = success;
+            return Builder;
+        }
+
+        public TBuilder Message(string message)
+        {
+            Result.Message = message;
+            return Builder;
+        }
+        public TResult Build()
+        {
+            return Result;
+        }
+
+    }
+}
