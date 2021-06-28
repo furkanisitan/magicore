44c95ea1dc85fcf447c474c7dbbf0e2264dcbd0d
Furkan Işıtan <furkanisitan96@gmail.com>
2021-06-28 09:52:37 +0300

    Create ResultTests.cs

---
+++ tests/Core.Tests/Utilities/Results/ResultTests.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Results;
+using NUnit.Framework;
+
+namespace Core.Tests.Utilities.Results
+{
+    [TestFixture]
+    public class ResultTests
+    {
+        [TestCase(false, "Test message.")]
+        public void Builder_WithoutData_AllPropertiesOfObjectsAreEqual(bool success, string message)
+        {
+            var resultNew = new Result { Success = success, Message = message };
+
+            var resultBuilder = Result.Builder().Success(success).Message(message).Build();
+
+            Assert.AreEqual(resultNew, resultBuilder);
+        }
+
+
+    }
+}
