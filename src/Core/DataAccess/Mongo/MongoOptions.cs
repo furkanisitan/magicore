850e79758220a2eaa217bac23a15ac492ae13b3c
furkanisitan <furkanisitan96@gmail.com>
2021-12-01 01:29:03 +0300

    Implement IRepository interface with MongoRepository class

---
+++ src/Core/DataAccess/Mongo/MongoOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Core.DataAccess.Mongo
+{
+    public class MongoOptions
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public Dictionary<string, string> CollectionNames { get; set; }
+    }
+}
---
+++ src/Core/DataAccess/Mongo/MongoRepository.cs
@@ -0,0 +1,54 @@
+﻿using Core.Entities;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Core.DataAccess.Mongo
+{
+    public abstract class MongoRepository<TEntity, TKey> : IRepository<TEntity, TKey>
+        where TEntity : class, IEntity<TKey>, new()
+        where TKey : IEquatable<TKey>
+    {
+        private readonly IMongoCollection<TEntity> _collection;
+
+        protected MongoRepository(IOptions<MongoOptions> options, string collectionNameKey)
+        {
+            var client = new MongoClient(options.Value.ConnectionString);
+            var database = client.GetDatabase(options.Value.DatabaseName);
+            _collection = database.GetCollection<TEntity>(options.Value.CollectionNames[collectionNameKey]);
+        }
+
+        public ICollection<TEntity> FindAll(Expression<Func<TEntity, bool>> predicate) =>
+            _collection.Find(predicate).ToList();
+
+        public ICollection<TEntity> FindAll() =>
+            FindAll(x => true);
+
+        public TEntity Find(Expression<Func<TEntity, bool>> predicate) =>
+            _collection.Find(predicate).FirstOrDefault();
+
+        public void Add(TEntity entity) =>
+            _collection.InsertOne(entity);
+
+        public void Update(TEntity entity) =>
+            _collection.ReplaceOne(x => x.Id.Equals(entity.Id), entity);
+
+        public void Delete(TEntity entity) =>
+            _collection.DeleteOne(x => x.Id.Equals(entity.Id));
+
+        public bool Exists(Expression<Func<TEntity, bool>> predicate) =>
+            _collection.AsQueryable().Any(predicate);
+
+        public TEntity FindById(TKey id) =>
+            Find(x => x.Id.Equals(id));
+
+        public void DeleteById(TKey id) =>
+            Delete(new TEntity { Id = id });
+
+        public bool ExistsById(TKey id) =>
+            Exists(x => x.Id.Equals(id));
+    }
+}
