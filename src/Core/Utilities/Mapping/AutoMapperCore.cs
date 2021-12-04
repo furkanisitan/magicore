d51e65ab680b33e16c0f7c50b4d43184ab031a01
furkanisitan <furkanisitan96@gmail.com>
2021-12-04 23:59:14 +0300

    Add mapping support

---
+++ src/Core/Utilities/Mapping/AutoMapperCore.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace Core.Utilities.Mapping
+{
+    public class AutoMapperCore : IMapperCore
+    {
+        private readonly IMapper _mapper;
+
+        public AutoMapperCore(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public TDestination Map<TSource, TDestination>(TSource source) =>
+         _mapper.Map<TDestination>(source);
+
+    }
+}
---
+++ src/Core/Utilities/Mapping/MapsterMapperCore.cs
@@ -0,0 +1,11 @@
+﻿using Mapster;
+
+namespace Core.Utilities.Mapping
+{
+    public class MapsterMapperCore : IMapperCore
+    {
+        public TDestination Map<TSource, TDestination>(TSource source) =>
+            source.Adapt<TDestination>();
+
+    }
+}
