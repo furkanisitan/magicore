e3e809507549061d9aa22466b0ef2c7e4ec75d92
furkanisitan <furkanisitan96@gmail.com>
2021-12-05 00:09:15 +0300

    Update Map method of MapsterMapperCore class

--- src/Core/Utilities/Mapping/MapsterMapperCore.cs
+++ src/Core/Utilities/Mapping/MapsterMapperCore.cs
@@ -1,11 +1,18 @@
-﻿using Mapster;
+﻿using MapsterMapper;
 
 namespace Core.Utilities.Mapping
 {
     public class MapsterMapperCore : IMapperCore
     {
+        private readonly IMapper _mapper;
+
+        public MapsterMapperCore(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
         public TDestination Map<TSource, TDestination>(TSource source) =>
-            source.Adapt<TDestination>();
+            _mapper.Map<TDestination>(source);
 
     }
 }
