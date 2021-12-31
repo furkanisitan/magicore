428793c56cd84f9d93b744419c56818b1b460b49
furkanisitan <furkanisitan96@gmail.com>
2022-01-01 01:51:24 +0300

    Test the Core.Utilities.Results project

---
+++ tests/Core/Utilities/Core.Utilities.Results.Tests/ResultTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Core.Utilities.Results.Tests;
+
+[TestFixture]
+public class ResultTests
+{
+    [TestCase(false, "Test message.")]
+    public void Builder_WithoutData_AllPropertiesOfObjectsAreEqual(bool success, string message)
+    {
+        var resultWithNew = new Result { Success = success, Message = message };
+
+        var resultWithBuilder = Result.Builder().Success(success).Message(message).Build();
+
+        Assert.AreEqual(resultWithNew.Message, resultWithBuilder.Message);
+        Assert.AreEqual(resultWithNew.Success, resultWithBuilder.Success);
+    }
+
+    [TestCase(false, "Test message.", 22)]
+    [TestCase(true, "Test message 2.", "data")]
+    public void Builder_WithData_AllPropertiesOfObjectsAreEqual<T>(bool success, string message, T data)
+    {
+        var resultWithNew = new Result<T> { Success = success, Message = message, Data = data };
+
+        var resultWithBuilder = Result.Builder(data).Success(success).Message(message).Build();
+
+        Assert.AreEqual(resultWithNew.Message, resultWithBuilder.Message);
+        Assert.AreEqual(resultWithNew.Success, resultWithBuilder.Success);
+        Assert.AreEqual(resultWithNew.Data, resultWithBuilder.Data);
+    }
+}
