ab3cf2343dc113d594811768432b9f341f10e465
furkanisitan <furkanisitan96@gmail.com>
2022-01-02 01:02:59 +0300

    Add Core.DI project

---
+++ src/Core/Core.DI/ServiceLocator.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.DI;
+
+public static class ServiceLocator
+{
+    private static IServiceProvider? _serviceProvider;
+
+    public static void Configure(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public static IServiceProvider GetServiceProvider() =>
+        _serviceProvider ?? throw new InvalidOperationException("ServiceProvider is not configured. Use the ServiceLocator.Configure() method in your application's startup file.");
+}
