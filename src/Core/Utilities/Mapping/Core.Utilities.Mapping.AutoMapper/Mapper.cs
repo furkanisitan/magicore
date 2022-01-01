a1604d91e4736b83f6cb03d3d99fcd271bf611d6
furkanisitan <furkanisitan96@gmail.com>
2022-01-01 13:09:10 +0300

    Add mapping projects

---
+++ src/Core/Utilities/Mapping/Core.Utilities.Mapping.AutoMapper/Mapper.cs
@@ -0,0 +1,19 @@
+﻿using AM = AutoMapper;
+
+namespace Core.Utilities.Mapping.AutoMapper;
+
+public class Mapper : IMapper
+{
+    private readonly AM.IMapper _mapper;
+
+    public Mapper(AM.IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    public TDestination Map<TDestination>(object source) =>
+        _mapper.Map<TDestination>(source);
+
+    public TDestination Map<TSource, TDestination>(TSource source) =>
+        _mapper.Map<TSource, TDestination>(source);
+}
---
+++ src/Core/Utilities/Mapping/Core.Utilities.Mapping.MapsterMapper/Mapper.cs
@@ -0,0 +1,19 @@
+﻿using MM = MapsterMapper;
+
+namespace Core.Utilities.Mapping.MapsterMapper;
+
+public class Mapper : IMapper
+{
+    private readonly MM.IMapper _mapper;
+
+    public Mapper(MM.IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    public TDestination Map<TDestination>(object source) =>
+        _mapper.Map<TDestination>(source);
+
+    public TDestination Map<TSource, TDestination>(TSource source) =>
+        _mapper.Map<TSource, TDestination>(source);
+}
