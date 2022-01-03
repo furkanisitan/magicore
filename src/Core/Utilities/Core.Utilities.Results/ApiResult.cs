79adccc58697f4fc49e666838f1fd3235896cd7e
furkanisitan <furkanisitan96@gmail.com>
2022-01-03 23:30:14 +0300

    Create ApiResult.cs

---
+++ src/Core/Utilities/Core.Utilities.Results/ApiResult.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public static class ApiResult
+    {
+        public const string SuccessOk = " The request has been processed successfully.";
+        public const string SuccessCreated = "Resource(s) added successfully.";
+        public const string ErrNotFound = "The resource not found.";
+        public const string ErrValidation = "A validation error has occurred.";
+        public const string ErrUniqueConstraint = "A unique constraint error has occurred.";
+        public const string ErrForeignKeyConstraint = "A foreign key constraint error has occurred.";
+
+        public static IResult Ok() =>
+            Result.Builder().Success().Message(SuccessOk).Build();
+
+        public static IResult<T> Created<T>(T payload) =>
+            Result.Builder(payload).Success().Message(SuccessCreated).Build();
+
+        public static IResult NotFound(string? name, params KeyValuePair<string, object>[] parameters) =>
+            Result.Builder().Message(ErrNotFound).AddError(BuildNotFoundMessage(name, parameters)).Build();
+
+        private static string BuildNotFoundMessage(string? name, params KeyValuePair<string, object>[] parameters)
+        {
+            if (name == null) return string.Empty;
+
+            var builder = new StringBuilder(name + " not found");
+
+            if (parameters.Length > 0)
+                builder.Append(" for parameters");
+
+            foreach (var (key, value) in parameters)
+                builder.Append($" {{{key}='{value}'}}");
+
+            return builder.Append('.').ToString();
+        }
+    }
+}
