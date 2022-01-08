96b55b06e39826f71652bad2692012ebf9bb5dc0
furkanisitan <furkanisitan96@gmail.com>
2022-01-08 20:41:44 +0300

    Update ValidationException.cs in Core.Validation project

--- src/Core/Validation/Core.Validation/ValidationException.cs
+++ src/Core/Validation/Core.Validation/ValidationException.cs
@@ -6,10 +6,40 @@
 public class ValidationException : Exception
 {
     /// <summary>
-    /// Creates a new ValidationException
+    /// Validation errors.
+    /// </summary>
+    public IEnumerable<string> Errors { get; }
+
+    /// <summary>
+    /// Creates a new ValidationException.
     /// </summary>
     /// <param name="message"></param>
-    public ValidationException(string message) : base(message)
+    public ValidationException(string message) : this(message, Enumerable.Empty<string>())
     {
+    }
+
+    /// <summary>
+    /// Creates a new ValidationException.
+    /// </summary>
+    /// <param name="errors"></param>
+    public ValidationException(IEnumerable<string> errors) : base(BuildErrorMessage(errors))
+    {
+        Errors = errors;
+    }
+
+    /// <summary>
+    /// Creates a new ValidationException.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="errors"></param>
+    public ValidationException(string message, IEnumerable<string> errors) : base(message)
+    {
+        Errors = errors;
+    }
+
+    private static string BuildErrorMessage(IEnumerable<string> errors)
+    {
+        var arr = errors.Select(x => $"{Environment.NewLine} -- {x}");
+        return "Validation failed: " + string.Join(string.Empty, arr);
     }
 }
