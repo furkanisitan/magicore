c51997106d0780fbc31fd01f583b651d1d247315
furkanisitan <furkanisitan96@gmail.com>
2022-01-12 01:00:57 +0300

    Add Core.API project

--- src/Core/Core.Utilities.Messaging/ApiResultMessages.cs
+++ src/Core/Core.Utilities.Messaging/ApiResultMessages.cs
@@ -5,6 +5,7 @@ public static class ApiResultMessages
     public const string Ok = " The request has been processed successfully.";
     public const string Created = "Resource(s) added successfully.";
 
+    public const string ErrBadRequest = "Invalid bad request.";
     public const string ErrNotFound = "The resource not found.";
     public const string ErrValidation = "A validation error has occurred.";
     public const string ErrUniqueConstraint = "A unique constraint error has occurred.";
--- src/Core/Utilities/Core.Utilities.Results/ApiResultHelper.cs
+++ src/Core/Utilities/Core.Utilities.Results/ApiResultHelper.cs
@@ -2,7 +2,7 @@
 
 namespace Core.Utilities.Results;
 
-public static class ApiResult
+public static class ApiResultHelper
 {
     public static IResult Ok() =>
         Result.Builder().Success().Message(ApiResultMessages.Ok).Build();
@@ -12,6 +12,12 @@ public static class ApiResult
 
     public static IResult<T> Created<T>(T payload) =>
         Result.Builder(payload).Success().Message(ApiResultMessages.Created).Build();
+
+    public static IResult BadRequest<T>(string? message = null) =>
+        Result.Builder().Success().Message(message ?? ApiResultMessages.ErrBadRequest).Build();
+
+    public static IResult<T> BadRequest<T>(T payload, string? message = null) =>
+        Result.Builder(payload).Success().Message(message ?? ApiResultMessages.ErrBadRequest).Build();
 
     public static IResult NotFound(string? name, params KeyValuePair<string, object>[] parameters) =>
         Result.Builder().Message(ApiResultMessages.ErrNotFound).AddError(MessageHelper.BuildNotFoundMessage(name, parameters)).Build();
