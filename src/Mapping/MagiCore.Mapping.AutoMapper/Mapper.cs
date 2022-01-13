bdaa229e6e1e264dc793ea904b697a553e676414
furkanisitan <furkanisitan96@gmail.com>
2022-01-14 00:36:15 +0300

    Update IMapper methods

--- src/Mapping/MagiCore.Mapping.AutoMapper/Mapper.cs
+++ src/Mapping/MagiCore.Mapping.AutoMapper/Mapper.cs
@@ -11,6 +11,18 @@ public class Mapper : IMapper
         _mapper = mapper;
     }
 
+    public TDestination Map<TDestination>(object? source) =>
+        _mapper.Map<TDestination>(source);
+
     public TDestination Map<TSource, TDestination>(TSource source) =>
         _mapper.Map<TSource, TDestination>(source);
+
+    public TDestination Map<TSource, TDestination>(TSource source, TDestination destination) =>
+        _mapper.Map(source, destination);
+
+    public object Map(object source, Type sourceType, Type destinationType) =>
+        _mapper.Map(source, sourceType, destinationType);
+
+    public object Map(object source, object destination, Type sourceType, Type destinationType) =>
+        _mapper.Map(source, destination, sourceType, destinationType);
 }
