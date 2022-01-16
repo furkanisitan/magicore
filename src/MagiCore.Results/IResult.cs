0e92a1f7199994453ab35238abbb584980590f6b
furkanisitan <furkanisitan96@gmail.com>
2022-01-16 22:10:54 +0300

    Update IResult.cs

--- src/MagiCore.Results/IResult.cs
+++ src/MagiCore.Results/IResult.cs
@@ -4,6 +4,7 @@ public interface IResult : ISuccessResult, IErrorResult
 {
     new bool Success { get; set; }
     new string? Message { get; set; }
+    new ICollection<string>? Errors { get; set; }
 }
 
 public interface IResult<T> : IResult, ISuccessResult<T>, IErrorResult<T>
