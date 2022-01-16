aa47849c961b66fa2b60fe2ad8524fa2db880400
furkanisitan <furkanisitan96@gmail.com>
2022-01-16 15:41:41 +0300

    Add MagiCore.DI project

---
+++ src/MagiCore.DI/ServiceLocator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MagiCore.DI;
+
+public static class ServiceLocator
+{
+    private static IServiceProvider? _serviceProvider;
+
+    public static void Configure(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public static IServiceProvider GetServiceProvider() =>
+        _serviceProvider ?? throw new InvalidOperationException("ServiceProvider is not configured. Use the ServiceLocator.Configure() method in your application's startup file.");
+
+    /// <inheritdoc cref="IServiceProvider.GetService" />
+    public static object? GetService(Type serviceType) =>
+        GetServiceProvider().GetService(serviceType);
+
+    /// <inheritdoc cref="ServiceProviderServiceExtensions.GetService{T}" />
+    public static T? GetService<T>() =>
+         GetServiceProvider().GetService<T>();
+
+    /// <inheritdoc cref="ServiceProviderServiceExtensions.GetRequiredService" />
+    public static object GetRequiredService(Type serviceType) =>
+        GetServiceProvider().GetRequiredService(serviceType);
+
+    /// <inheritdoc cref="ServiceProviderServiceExtensions.GetRequiredService{T}" />
+    public static T GetRequiredService<T>() where T : notnull =>
+        GetServiceProvider().GetRequiredService<T>();
+}
