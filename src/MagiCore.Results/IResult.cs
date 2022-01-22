78decbb6e0d8be2aebd49c536a391c101e269744
furkanisitan <furkanisitan96@gmail.com>
2022-01-22 19:55:08 +0300

    Remove ISuccessResult and IErrorResult

--- src/MagiCore.Results/IResult.cs
+++ src/MagiCore.Results/IResult.cs
@@ -1,13 +1,13 @@
 ﻿namespace MagiCore.Results;
 
-public interface IResult : ISuccessResult, IErrorResult
+public interface IResult
 {
-    new bool Success { get; set; }
-    new string? Message { get; set; }
-    new ICollection<string>? Errors { get; set; }
+    bool Success { get; set; }
+    string? Message { get; set; }
+    ICollection<string>? Errors { get; set; }
 }
 
-public interface IResult<T> : IResult, ISuccessResult<T>, IErrorResult<T>
+public interface IResult<T> : IResult
 {
-    new T? Payload { get; set; }
+    T? Payload { get; set; }
 }
